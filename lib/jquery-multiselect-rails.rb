dc5c6c9b48d2734b5236293ad733618e2f647d63
Alex Rojo <arojo@gynaika.net>
2013-01-10 23:44:03 +0100

    trying to find the solution to get gem assets includes in rails pipe

--- lib/jquery-multiselect-rails.rb
+++ lib/jquery-multiselect-rails.rb
@@ -1,11 +1,2 @@
 require "jquery-multiselect-rails/version"
-
-module Jquery
-  module Multiselect
-    module Rails
-	  class Engine < Rails::Engine
-
-	  end
-    end
-  end
-end
+require "jquery-multiselect-rails/engine"
---
+++ lib/jquery-multiselect-rails/engine.rb
@@ -0,0 +1,11 @@
+# Configure for Rails 3.1
+module Jquery
+  module Multiselect
+    if defined?(::Rails) and ::Rails.version >= "3.1"
+      module Rails
+        class Engine < ::Rails::Engine
+        end
+      end
+    end
+  end
+end
